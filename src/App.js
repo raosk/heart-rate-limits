34d1abe5a8e58eb7dbe0c60914b23fc611ca1591
raosk <oskari.rantamaula@gmail.com>
2022-09-14 20:14:26 +0300

    all is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [age, setAge] = useState(0);
+  const [lowerLimit, setLowerLimit] = useState(0);
+  const [upperLimit, setUpperLimit] = useState(0);
+
+  function calculate() {
+    setUpperLimit((220 - age) * 0.85);
+    setLowerLimit((220 - age) * 0.65);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <form onSubmit={e => { e.preventDefault(); }}>
+        <h3>Heart rate limits calculator</h3>
+
+        <div>
+          <label>Age</label>
+        </div>
+        <div>
+          <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+        </div>
+
+        <div>
+          <label>Heart rate limits</label>
+        </div>
+        <div>
+          <output id="result-box">{lowerLimit.toFixed(0)}-{upperLimit.toFixed(0)}</output>
+        </div>
+
+        <div>
+          <button type="button" onClick={calculate}>Calculate</button>
+        </div>
+
+      </form>
     </div>
   );
 }
